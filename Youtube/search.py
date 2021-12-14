b17c99fd5af4b985c21666ac0b8c5cd317b9cb79
Suvapna <suvapnamaheswaran@gmx.de>
2021-12-14 14:14:55 +0100

    neuer Ordner

---
+++ Youtube/search.py
@@ -0,0 +1,12 @@
+import urllib.request
+import re
+
+search_keyword = "%23laugh"
+html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
+video_ids = re.findall(r"watch\?v=(\S{11})",html.read().decode())
+
+x = 0
+for video_id in video_ids:  
+    print("https://www.youtube.com/watch?v=" + video_ids[x])
+    x += 1
+    
